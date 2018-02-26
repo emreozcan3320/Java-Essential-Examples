f51b7f4ae400c4985846362e7cff2c294aad1c2b
emreozcan3320 <kadiremreozcan@gmail.com>
2018-02-26 19:55:17 +0300

    Exapmle File Structure Prepared

---
+++ src/ArrayList/Main.java
@@ -0,0 +1,14 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> arraylist = new ArrayList<String>();
+        arraylist.add("python");
+        arraylist.add("java");
+        arraylist.add("c++");
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<String> arraylist = new ArrayList<String>();
+        arraylist.add("python");
+        arraylist.add("java");
+        arraylist.add("c++");
+    }
+}
